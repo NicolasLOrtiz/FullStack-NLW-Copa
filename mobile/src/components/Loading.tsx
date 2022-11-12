8b5092bb65ccbfd889112f7fe79bbe17df60d8e5
NicolasLOrtiz <contato.nicolol@gmail.com>
2022-11-12 11:12:09 -0300

    agora vai

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import {Center, Spinner} from "native-base";
+
+
+export const Loading = () => {
+  return (
+    <Center flex={1} bg={"gray.900"}>
+      <Spinner accessibilityLabel="Loading posts" color={"yellow.500"}/>
+    </Center>
+  );
+};
